6207f27ac58f8b1b6181292c7910a7933940fb31
Tim Morgan <tim@timmorgan.org>
2021-02-28 08:43:22 -0600

    Fix bug with arguments in Ruby 3

--- lib/pco/api.rb
+++ lib/pco/api.rb
@@ -4,8 +4,8 @@ require_relative 'api/errors'
 module PCO
   module API
     module_function
-    def new(*args)
-      Endpoint.new(*args)
+    def new(**args)
+      Endpoint.new(**args)
     end
   end
 end
